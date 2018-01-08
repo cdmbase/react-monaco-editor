0c2d6bbf86549620c0393cd0b30885c938398bfe
veeramarni <mvasireddy@cdmbase.com>
2018-01-08 00:04:21 -0500

    update

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,145 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import 'reflect-metadata';
+const { Container } = require('inversify');
+import { MonacoEditorProvider} from '@theia/monaco/lib/browser';
 import React from 'react';
 import { render } from 'react-dom';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import MonacoEditor from 'react-monaco-editor';
 /* eslint-enable import/no-extraneous-dependencies */
 
-// Using with webpack
-class CodeEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      code: '// type your code... \n',
-    }
-  }
-
-  onChange = (newValue, e) => {
-    console.log('onChange', newValue, e); // eslint-disable-line no-console
-  }
-
-  editorDidMount = (editor) => {
-    // eslint-disable-next-line no-console
-    console.log('editorDidMount', editor, editor.getValue(), editor.getModel());
-    this.editor = editor;
-  }
-
-  changeEditorValue = () => {
-    if (this.editor) {
-      this.editor.setValue('// code changed! \n');
-    }
-  }
-
-  changeBySetState = () => {
-    this.setState({ code: '// code changed by setState! \n' });
-  }
-
-  render() {
-    const code = this.state.code;
-    const options = {
-      selectOnLineNumbers: true,
-      roundedSelection: false,
-      readOnly: false,
-      cursorStyle: 'line',
-      automaticLayout: false,
-    };
-    return (
-      <div>
-        <div>
-          <button onClick={this.changeEditorValue}>Change value</button>
-          <button onClick={this.changeBySetState}>Change by setState</button>
-        </div>
-        <hr />
-        <MonacoEditor
-          height="500"
-          language="javascript"
-          value={code}
-          options={options}
-          onChange={this.onChange}
-          editorDidMount={this.editorDidMount}
-        />
-      </div>
-    );
-  }
+const container = new Container();
+function load(raw) {
+  return Promise.resolve(raw.default).then(module =>
+    container.load(module)
+  )
 }
 
-// Using with require.config
-class AnotherEditor extends React.Component { // eslint-disable-line react/no-multi-comp
-  constructor(props) {
-    super(props);
-    const jsonCode = [
-      '{',
-      '    "$schema": "http://myserver/foo-schema.json"',
-      '}'
-    ].join('\n');
-    this.state = {
-      code: jsonCode,
-    }
-  }
-
-  editorWillMount = (monaco) => {
-    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-      schemas: [{
-        uri: 'http://myserver/foo-schema.json',
-        schema: {
-          type: 'object',
-          properties: {
-            p1: {
-              enum: [ 'v1', 'v2']
-            },
-            p2: {
-              $ref: 'http://myserver/bar-schema.json'
-            }
-          }
-        }
-      }, {
-        uri: 'http://myserver/bar-schema.json',
-        schema: {
-          type: 'object',
-          properties: {
-            q1: {
-              enum: [ 'x1', 'x2']
-            }
-          }
-        }
-      }]
-    });
-  }
-  render() {
-    const code = this.state.code;
-    const requireConfig = {
-      url: 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.1/require.min.js',
-      paths: {
-        vs: 'https://as.alipayobjects.com/g/cicada/monaco-editor-mirror/0.6.1/min/vs'
-      }
-    };
-    return (
-      <div>
-        <MonacoEditor
-          width="800"
-          height="600"
-          language="json"
-          defaultValue={code}
-          requireConfig={requireConfig}
-          editorWillMount={this.editorWillMount}
-        />
-      </div>
-    );
-  }
+function start() {
+  const editorProvider = container.get(MonacoEditorProvider);
+  console.log(editorProvider);
 }
 
-// eslint-disable-next-line react/no-multi-comp
-const App = () => (
-  <div>
-    <h2>Monaco Editor Sample (controlled mode)</h2>
-    <CodeEditor />
-    <hr />
-    <h2>Another editor (uncontrolled mode)</h2>
-    <AnotherEditor />
-  </div>
-)
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+Promise.resolve()
+  .then(function () { return Promise.resolve(require('@theia/monaco/lib/browser/monaco-browser-module')).then(load) })
+  .then(start).catch(reason => {
+    console.error('Failed to start the frontend application.');
+    if (reason) {
+      console.error(reason);
+    }
+  });
+
+
+
+
--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -1,14 +1,34 @@
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
 const path = require('path');
 
+const development = process.env.NODE_ENV === 'development';
+
 const MonacoEditorSrc = path.join(__dirname, '..', '..', 'src');
+
+const monacoEditorPath = development ? 'node_modules/monaco-editor-core/dev/vs' : 'node_modules/monaco-editor-core/min/vs';
+const monacoLanguagesPath = 'node_modules/monaco-languages/release';
+const monacoCssLanguagePath = 'node_modules/monaco-css/release/min';
+const monacoJsonLanguagePath = 'node_modules/monaco-json/release/min';
+const monacoHtmlLanguagePath = 'node_modules/monaco-html/release/min';
+
+const requirePath = 'node_modules/requirejs/require.js';
+const outputPath = path.join(__dirname, './lib/t');
 
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.join(__dirname, './lib/t'),
+    path: outputPath,
     filename: 'index.js',
+  },
+  target: 'web',
+  node: {
+    fs: 'empty',
+    child_process: 'empty',
+    net: 'empty',
+    crypto: 'empty'
   },
   module: {
     rules: [
@@ -17,15 +37,48 @@ module.exports = {
         use: ['file?name=[name].[ext]'],
       },
       {
+        test: /\.css$/,
+        exclude: /\.useable\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.useable\.css$/,
+        loader: 'style-loader/useable!css-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
+      {
+        test: /.(jpg|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[hash].[ext]',
+        }
+      },
+      {
+        // see https://github.com/theia-ide/theia/issues/556
+        test: /source-map-support/,
+        loader: 'ignore-loader'
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [{ loader: 'react-hot-loader/webpack' }, { loader: 'babel-loader' }]
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      }
     ],
+    noParse: /vscode-languageserver-types|vscode-uri|jsonc-parser/
   },
   resolve: {
     extensions: ['.js', '.json'],
-    alias: { 'react-monaco-editor': MonacoEditorSrc }
+    alias: {
+      'react-monaco-editor': MonacoEditorSrc,
+      'vs': path.resolve(outputPath, monacoEditorPath)
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -34,10 +87,36 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') },
     }),
-    new CopyWebpackPlugin([{
-      from: 'node_modules/monaco-editor/min/vs',
-      to: 'vs',
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: requirePath,
+        to: '.'
+      },
+      {
+        from: monacoEditorPath,
+        to: 'vs'
+      },
+      {
+        from: monacoLanguagesPath,
+        to: 'vs/basic-languages'
+      },
+      {
+        from: monacoCssLanguagePath,
+        to: 'vs/language/css'
+      },
+      {
+        from: monacoJsonLanguagePath,
+        to: 'vs/language/json'
+      },
+      {
+        from: monacoHtmlLanguagePath,
+        to: 'vs/language/html'
+      }
+    ]),
+    new CircularDependencyPlugin({
+      exclude: /(node_modules|examples)\/./,
+      failOnError: false // https://github.com/nodejs/readable-stream/issues/280#issuecomment-297076462
+    })
   ],
   devServer: { contentBase: './' }
 }
